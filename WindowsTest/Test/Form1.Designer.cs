76088f368aca18d38f2d8cf043086891825f9e9a
welkin.hu <welkin.hu@f3e0109f-1e46-0410-ab28-4f22c37eb7d4>
2009-06-18 06:12:19 +0000

    update to 1.0.0.6

--- WindowsTest/Test/Form1.Designer.cs
+++ WindowsTest/Test/Form1.Designer.cs
@@ -23,41 +23,50 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.spinEdit1 = new DevExpress.XtraEditors.SpinEdit();
-            ((System.ComponentModel.ISupportInitialize)(this.spinEdit1.Properties)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // spinEdit1
-            // 
-            this.spinEdit1.EditValue = new decimal(new int[] {
-            2,
+			this.spinEdit1 = new DevExpress.XtraEditors.SpinEdit();
+			this.monitorServiceButton = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.spinEdit1.Properties)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// spinEdit1
+			// 
+			this.spinEdit1.EditValue = new decimal(new int[] {
+            0,
             0,
             0,
             0});
-            this.spinEdit1.Location = new System.Drawing.Point(42, 71);
-            this.spinEdit1.Name = "spinEdit1";
-            this.spinEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
-            new DevExpress.XtraEditors.Controls.EditorButton()});
-            this.spinEdit1.Properties.DisplayFormat.FormatString = "P0";
-            this.spinEdit1.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric;
-            this.spinEdit1.Size = new System.Drawing.Size(161, 20);
-            this.spinEdit1.TabIndex = 0;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 263);
-            this.Controls.Add(this.spinEdit1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.spinEdit1.Properties)).EndInit();
-            this.ResumeLayout(false);
+			this.spinEdit1.Location = new System.Drawing.Point(0, 0);
+			this.spinEdit1.Name = "spinEdit1";
+			this.spinEdit1.Size = new System.Drawing.Size(100, 20);
+			this.spinEdit1.TabIndex = 0;
+			// 
+			// monitorServiceButton
+			// 
+			this.monitorServiceButton.Location = new System.Drawing.Point(41, 49);
+			this.monitorServiceButton.Name = "monitorServiceButton";
+			this.monitorServiceButton.Size = new System.Drawing.Size(160, 40);
+			this.monitorServiceButton.TabIndex = 1;
+			this.monitorServiceButton.Text = "Monitor Service";
+			this.monitorServiceButton.UseVisualStyleBackColor = true;
+			this.monitorServiceButton.Click += new System.EventHandler(this.monitorServiceButton_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(292, 263);
+			this.Controls.Add(this.monitorServiceButton);
+			this.Controls.Add(this.spinEdit1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.spinEdit1.Properties)).EndInit();
+			this.ResumeLayout(false);
 
         }
 
         #endregion
 
         private DevExpress.XtraEditors.SpinEdit spinEdit1;
+		private System.Windows.Forms.Button monitorServiceButton;
     }
 }
